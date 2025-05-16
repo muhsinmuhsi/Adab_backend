33c9b265f6a79b49c0d6eb78de28b378a6e56229
muhzin ck <muhsinmuhsinmuhsi786@gmail.com>
2025-05-16 11:15:32 +0530

    admin route prodtected

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,9 +2,11 @@ import express from 'express'
 import {adminAddProduct, deleteProduct, updateProduct}  from '../controllers/adminProductControllers.js'
 import uploadImage from '../middleware/uploadImage.js';
 import { Login } from '../controllers/adminAuth.js';
+import { Adminauthmidd } from '../middleware/authMidd.js';
 
 const route=express.Router()
 route.post('/Login',Login);
+route.use(Adminauthmidd);
 route.post('/products',uploadImage,adminAddProduct);
 route.put('/products/:id',uploadImage,updateProduct);
 route.delete('/products/:id',deleteProduct);
